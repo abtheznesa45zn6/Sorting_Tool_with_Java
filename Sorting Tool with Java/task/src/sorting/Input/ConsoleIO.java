c528677595f4b585b511a884ff1cab33d88f9ca1
abtheznesa45zn6 <errror4224@gmail.com>
2024-03-10 10:40:24 +0100

    Stage 3/6: Sorting it out

--- Sorting Tool with Java/task/src/sorting/Input/ConsoleIO.java
+++ Sorting Tool with Java/task/src/sorting/Input/ConsoleIO.java
@@ -2,49 +2,47 @@ package sorting.Input;
 
 import java.util.*;
 
-public class ConsoleInput<T extends Comparable<T>> {
+public class ConsoleIO<T extends Comparable<T>> {
     private final DataType dataType;
     private final ArrayList<T> data;
+    private final boolean sorted;
 
-    private ConsoleInput(DataType dataType, ArrayList<T> data) {
+    private ConsoleIO(DataType dataType, ArrayList<T> data, boolean sorted) {
         this.dataType = dataType;
         this.data = data;
+        this.sorted = sorted;
     }
 
-    public static ConsoleInput<?> getInstance(String[] args) {
+    public static ConsoleIO<?> getInstance(DataType type, boolean sorted) {
         try(Scanner scanner = new Scanner(System.in)) {
-            if (args.length > 0 && args[0].equals("-dataType")) {
-                if (args[1].equals("long")) {
-                    return ConsoleInput.getInputLONG(scanner);
-                }
-                if (args[1].equals("line")) {
-                    return ConsoleInput.getInputLINE(scanner);
-                }
-            }
-            return ConsoleInput.getInputWORD(scanner);
+            return switch (type) {
+                case LONG -> ConsoleIO.getInputLONG(scanner, sorted);
+                case LINE -> ConsoleIO.getInputLINE(scanner, sorted);
+                case WORD -> ConsoleIO.getInputWORD(scanner, sorted);
+            };
         }
     }
 
-    private static ConsoleInput<Long> getInputLONG(Scanner scanner) {
+    private static ConsoleIO<Long> getInputLONG(Scanner scanner, boolean sorted) {
         ArrayList<Long> input = new ArrayList<>();
         while (scanner.hasNext()) {
             input.add(scanner.nextLong());
         }
-        return new ConsoleInput<>(DataType.LONG, input);
+        return new ConsoleIO<>(DataType.LONG, input, sorted);
     }
-    private static ConsoleInput<String> getInputLINE(Scanner scanner) {
+    private static ConsoleIO<String> getInputLINE(Scanner scanner, boolean sorted) {
         ArrayList<String> input = new ArrayList<>();
         while (scanner.hasNext()) {
             input.add(scanner.nextLine());
         }
-        return new ConsoleInput<>(DataType.LINE, input);
+        return new ConsoleIO<>(DataType.LINE, input, sorted);
     }
-    private static ConsoleInput<String> getInputWORD(Scanner scanner) {
+    private static ConsoleIO<String> getInputWORD(Scanner scanner, boolean sorted) {
         ArrayList<String> input = new ArrayList<>();
         while (scanner.hasNext()) {
             input.add(scanner.next());
         }
-        return new ConsoleInput<>(DataType.WORD, input);
+        return new ConsoleIO<>(DataType.WORD, input, sorted);
     }
 
     private int getNumberOfElements() {
@@ -77,17 +75,38 @@ public class ConsoleInput<T extends Comparable<T>> {
 
     private void printLONG() {
         System.out.printf("Total numbers: %d.\n", getNumberOfElements());
-        System.out.printf("The greatest number: %s (%d time(s), %d%%).\n", getMax(), getFrequencyOfMax(), getFrequencyPercentage());
+        if (sorted){
+            System.out.printf("Sorted data:%s\n", getSortedDate());
+        } else {
+            System.out.printf("The greatest number: %s (%d time(s), %d%%).\n", getMax(), getFrequencyOfMax(), getFrequencyPercentage());
+        }
     }
 
     private void printLINE() {
         System.out.printf("Total lines: %d.\n", getNumberOfElements());
-        System.out.printf("The longest line:\n%s\n(%d time(s), %d%%).\n", getMax(), getFrequencyOfMax(), getFrequencyPercentage());
+        if (sorted){
+            System.out.printf("Sorted data:%s\n", getSortedDate());
+        } else {
+            System.out.printf("The longest line:\n%s\n(%d time(s), %d%%).\n", getMax(), getFrequencyOfMax(), getFrequencyPercentage());
+        }
     }
 
     private void printWORD() {
         System.out.printf("Total words: %d.\n", getNumberOfElements());
-        System.out.printf("The longest word: %s (%d time(s), %d%%).\n", getMax(), getFrequencyOfMax(), getFrequencyPercentage());
+        if (sorted){
+            System.out.printf("Sorted data:%s\n", getSortedDate());
+        } else {
+            System.out.printf("The longest word: %s (%d time(s), %d%%).\n", getMax(), getFrequencyOfMax(), getFrequencyPercentage());
+        }
+    }
+
+    private String getSortedDate() {
+        data.sort(new CustomComparatorForInteger());
+        StringBuilder output = new StringBuilder();
+        for (T s : data) {
+            output.append(" ").append(s.toString());
+        }
+        return output.toString();
     }
 
     // from ChatGPT, should've done different classes for Long/String
@@ -104,6 +123,19 @@ public class ConsoleInput<T extends Comparable<T>> {
                 return Long.compare(l1, l2);
             } else {
                 // Handle comparison between different types if needed
+                return 0;
+            }
+        }
+    }
+
+    private static class CustomComparatorForInteger implements Comparator<Object> {
+        @Override
+        public int compare(Object o1, Object o2) {
+            if (o1 instanceof String s1 && o2 instanceof String s2) {
+                return Long.compare(Long.parseLong(s1), Long.parseLong(s2));
+            } else if (o1 instanceof Long l1 && o2 instanceof Long l2) {
+                return Long.compare(l1, l2);
+            } else {
                 return 0;
             }
         }
--- Sorting Tool with Java/task/src/sorting/Main.java
+++ Sorting Tool with Java/task/src/sorting/Main.java
@@ -1,10 +1,28 @@
 package sorting;
 
-import sorting.Input.ConsoleInput;
+import sorting.Input.ConsoleIO;
+import sorting.Input.DataType;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(final String[] args) {
-        ConsoleInput<?> consoleInput = ConsoleInput.getInstance(args);
-        consoleInput.printStats();
+        ConsoleIO<?> consoleIO = ConsoleIO.getInstance(getDataTypeArgument(args), getSortedArgument(args));
+        consoleIO.printStats();
+    }
+
+    private static DataType getDataTypeArgument(String[] args) {
+        if (args.length > 0 && args[0].equals("-dataType")) {
+            if (args[1].equals("long")) {
+                return DataType.LONG;
+            }
+            if (args[1].equals("line")) {
+                return DataType.LINE;
+            }
+        }
+        return DataType.WORD;
+    }
+    private static boolean getSortedArgument(String[] args) {
+        return Arrays.asList(args).contains("-sortIntegers");
     }
 }
